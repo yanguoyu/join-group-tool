4038ea1e4a7a929af2d1281886ef0f6b51773ef6
yanguoyu <yanguoyu@betalpha.com>
2018-11-04 13:58:27 +0800

    1：忽略loading

--- src/store/api-middware.js
+++ src/store/api-middware.js
@@ -3,20 +3,20 @@ import { addInPromiseCount, delInPromiseCount, promiseError }  from '../actions/
 
 export default ({ dispatch, getState }) => next => action => {
   if(isPromise(action.payload)) {
-    dispatch(addInPromiseCount(action.type));
+    !action.ignoreLoading && dispatch(addInPromiseCount(action.type));
     action.payload.then(res => {
       next({
         ...action,
         payload: res.result
       })
-      dispatch(delInPromiseCount(action.type));
+      !action.ignoreLoading && dispatch(delInPromiseCount(action.type));
       if(action.handlers) {
         action.handlers.success && action.handlers.success(res.result, dispatch, getState());
         action.handlers.finish && action.handlers.finish(res.result, dispatch, getState());
       }
     }).catch(err=> {
-      dispatch(delInPromiseCount(action.type));
-      dispatch(promiseError(action.type));
+      !action.ignoreLoading && dispatch(delInPromiseCount(action.type));
+      !action.ignoreLoading && dispatch(promiseError(action.type));
       console.log(err);
       if(action.handlers) {
         action.handlers.failed && action.handlers.failed(err, dispatch, getState());
